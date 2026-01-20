0b1d398335368641e5185b4e3331f68f40a337c7
santoshmode07 <santoshmode7@gmail.com>
2026-01-20 22:46:39 +0530

    Handling Cast error and duplicate field errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,15 @@
+const AppError = require('./../Utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -21,8 +33,8 @@ const sendErrorProd = (err, res) => {
     console.error('ERROR 💥', err);
 
     // 2) Send generic message
-
     res.status(500).json({
+      status: 'error',
       message: 'Something went very wrong!',
     });
   }
@@ -31,10 +43,18 @@ const sendErrorProd = (err, res) => {
 module.exports = (err, req, res, next) => {
   //   console.log(err.stack);
   err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+    if (err.name === 'CastError') {
+      error = handleCastErrorDB(err);
+    }
+    if (err.code === 11000) {
+      error = handleDuplicateFieldsDB(err);
+    }
     // Operational, trusted error: send message to client
-    sendErrorProd(err, res);
+    sendErrorProd(error, res);
   }
 };
