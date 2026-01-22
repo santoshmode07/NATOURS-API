4b0f0a6f9376d7baef673940995e388df54530a2
santoshmode07 <santoshmode7@gmail.com>
2026-01-22 15:44:52 +0530

    Protecting Tour routes Part-1

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -8,7 +8,7 @@ router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router
   .route('/')
-  .get(userController.getAllUsers)
+  .get(authController.protect, userController.getAllUsers)
   .post(userController.createUser);
 
 router
