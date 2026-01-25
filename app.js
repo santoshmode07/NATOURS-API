6300df0d269f0390569e563b75ebdb4df88b09da
santoshmode07 <santoshmode7@gmail.com>
2026-01-25 19:03:10 +0530

    Implemeting data sanitization

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongosanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./Utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -31,6 +33,14 @@ app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json());
+
+//Data sanitization against NoSQL query injection
+
+app.use(mongosanitize());
+
+//Data Sanitization against XSS
+
+app.use(xss());
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
