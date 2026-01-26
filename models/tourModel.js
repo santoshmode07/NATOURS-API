8cb405b65dd9f2edc26b361cb8dbc330f0aed0d7
santoshmode07 <santoshmode7@gmail.com>
2026-01-26 23:07:40 +0530

    Populating the tour guides

--- models/tourModel.js
+++ models/tourModel.js
@@ -159,6 +159,14 @@ tourSchema.post(/^find/, function (docs, next) {
   next();
 });
 
+tourSchema.pre('/^find', function (next) {
+  this.populate({
+    path: 'guides',
+    select: '-__v -passwordChangedAt',
+  });
+  next();
+});
+
 //AGGREGATION MIDDLEWARE
 tourSchema.pre('aggregate', function (next) {
   this.pipeline().unshift({
