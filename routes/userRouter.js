1ef04a8ab9bda619b609b8f1de0f4fd89c88a664
santoshmode07 <santoshmode7@gmail.com>
2026-02-11 11:21:23 +0530

    Saving image name to Database

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const multer = require('multer');
 const userController = require('./../controllers/userController');
 const authController = require('./../controllers/authController');
 
-const upload = multer({ dest: 'public/img/users' });
 const router = express.Router();
 
 router.post('/signup', authController.signup);
@@ -17,7 +15,11 @@ router.use(authController.protect);
 
 router.patch('/updateMypassword', authController.updatePassword);
 router.get('/me', userController.getMe, userController.getUser);
-router.patch('/updateMe', upload.single('photo'), userController.updateMe);
+router.patch(
+  '/updateMe',
+  userController.uploadUserPhoto,
+  userController.updateMe,
+);
 router.delete('/deleteMe', userController.deleteMe);
 
 //Restrict all routes after this middleware to admin only
